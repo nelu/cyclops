138afe55540d48b6108dd81b736fa1424d396976
Chase Brewer <chase.brewer@dunbarcyber.com>
2017-09-12 09:42:15 -0400

    add tag display component

---
+++ src/app/services/tags/components/TagLabel.tsx
@@ -0,0 +1,46 @@
+/**
+ * The contents of this file are subject to the CYPHON Proprietary Non-
+ * Commercial Registered User Use License Agreement (the "Agreement”). You
+ * may not use this file except in compliance with the Agreement, a copy
+ * of which may be found at https://github.com/dunbarcyber/cyclops/. The
+ * developer of the CYPHON technology and platform is Dunbar Security
+ * Systems, Inc.
+ *
+ * The CYPHON technology or platform are distributed under the Agreement on
+ * an “AS IS” basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the Agreement for specific terms.
+ *
+ * Copyright (C) 2017 Dunbar Security Solutions, Inc. All Rights Reserved.
+ *
+ * Contributor/Change Made By: ________________. [Only apply if changes
+ * are made]
+ */
+
+// Vendor
+import * as React from 'react';
+
+// Local
+import { Tag } from '~/services/tags/types';
+
+// --------------------------------------------------------------------------
+// Interfaces/Types
+// --------------------------------------------------------------------------
+
+interface Props {
+  tag: Tag;
+}
+
+// --------------------------------------------------------------------------
+// Component
+// --------------------------------------------------------------------------
+
+/**
+ * Name of a tag object in a label.
+ */
+export class TagLabel extends React.Component<Props, {}> {
+  public render() {
+    return (
+      <div className="label label-primary">{this.props.tag.name}</div>
+    );
+  }
+}
