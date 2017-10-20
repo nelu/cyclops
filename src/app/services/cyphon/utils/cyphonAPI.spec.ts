7287c134bb5418ba98e416834b21d0377d96664c
Chase Brewer <chase.brewer@dunbarcyber.com>
2017-10-20 09:46:26 -0400

    fix duplicate monitor items in monitor panel

---
+++ src/app/services/cyphon/utils/cyphonAPI.spec.ts
@@ -0,0 +1,57 @@
+// Vendor
+import * as sinon from 'sinon';
+
+// Local
+import * as cyphonAPI from './cyphonAPI';
+import { cyphonAPI as api } from '../constants';
+
+describe('cyphonAPI', () => {
+  describe('getAll()', () => {
+    let request: sinon.SinonStub;
+
+    beforeEach(() => {
+      request = sinon.stub(api, 'request');
+    });
+
+    afterEach(() => {
+      request.restore();
+    });
+
+    it('should recursively fetch data from a url', () => {
+      request.onCall(0).resolves({
+        data: {
+          next: 'http://cyphon.io/api/v1/monitors/enabled/?page=2',
+          previous: null,
+          count: 3,
+          results: [1],
+        },
+      });
+
+      request.onCall(1).resolves({
+        data: {
+          next: 'http://cyphon.io/api/v1/monitors/enabled/?page=3',
+          previous: 'http://cyphon.io/api/v1/monitors/enabled/?page=1',
+          count: 3,
+          results: [2],
+        },
+      });
+
+      request.onCall(2).resolves({
+        data: {
+          next: null,
+          previous: 'http://cyphon.io/api/v1/monitors/enabled/?page=2',
+          count: 3,
+          results: [3],
+        },
+      });
+
+      return cyphonAPI.getAll('/monitors/enabled/').then((response) => {
+        expect(response).to.deep.equal([1, 2, 3]);
+        expect(request.callCount).to.equal(3);
+        expect(request.args[0][0].url).to.equal('/monitors/enabled/');
+        expect(request.args[1][0].url).to.equal('http://cyphon.io/api/v1/monitors/enabled/?page=2');
+        expect(request.args[2][0].url).to.equal('http://cyphon.io/api/v1/monitors/enabled/?page=3');
+      });
+    });
+  });
+});
--- src/app/services/cyphon/utils/cyphonAPI.ts
+++ src/app/services/cyphon/utils/cyphonAPI.ts
@@ -17,26 +17,11 @@
  */
 
 // Vendor
-import axios, { AxiosInstance } from 'axios';
-import * as _ from 'lodash';
-import * as Cookies from 'js-cookie';
+import { AxiosRequestConfig } from 'axios';
 
 // Local
-import { getConfig } from '~/config';
-import { APIConfig, APIList } from '../types';
-import { paramsSerializer } from './URLParams';
-import * as cache from './APICache';
-
-/**
- * Instance of axios that connects to the Express Cyphon API Proxy.
- * @type {AxiosInstance}
- */
-const cyphonAPI: AxiosInstance = axios.create({
-  baseURL: getConfig().API_URL,
-  paramsSerializer,
-  timeout: getConfig().API_TIMEOUT,
-  headers: { 'X-CSRFToken': Cookies.get('csrftoken') },
-});
+import { APIList } from '../types';
+import { cyphonAPI } from '../constants';
 
 /**
  * Makes a request to the CyphonAPI using an APIConfig object, creates a
@@ -46,34 +31,8 @@ const cyphonAPI: AxiosInstance = axios.create({
  * @param config Request configuration.
  * @returns {Promise<T>}
  */
-export function request<T>(config: APIConfig): Promise<T> {
-  const cacheRequest = (config.cache && config.method === 'get');
-
-  // Return the cached request if there is one and if cache is true.
-  if (cacheRequest) {
-    const cachedPromise = cache.get<T>(config);
-    if (cachedPromise) { return cachedPromise; }
-  }
-
-  const promise: Promise<T> = cyphonAPI.request(config)
-    // Extract that data from the successful response.
-    .then<T>((response) => response.data);
-
-  if (cacheRequest) {
-    // If cache is set to true and the promise fails, remove the promise
-    // from the cache.
-    const updatedPromise = promise.catch((error) => {
-      cache.remove(config);
-      return error;
-    });
-
-    // Save the promise to the cache.
-    cache.set(config, updatedPromise);
-
-    return updatedPromise;
-  }
-
-  return promise;
+export function request<T>(config: AxiosRequestConfig): Promise<T> {
+  return cyphonAPI.request(config).then<T>((response) => response.data);
 }
 
 /**
@@ -82,34 +41,31 @@ export function request<T>(config: APIConfig): Promise<T> {
  * @param config Request configuration.
  * @returns {Promise<T>}
  */
-export function get<T>(url: string, config?: APIConfig): Promise<T> {
-  return request<T>(_.assign({}, config, { url, method: 'get' }));
+export function get<T>(url: string, config: AxiosRequestConfig = {}): Promise<T> {
+  return request<T>({ ...config, url, method: 'get' });
 }
 
 /**
  * Recursively gets all the results from an get response that returns an
  * APIList response by paginating.
- * @param baseUrl Base url of the get request.
- * @param params Stringified parameters to add to the base url.
+ * @param url Base url of the get request.
  * @param results Previous results to add new results onto.
  * @returns {Promise<T[]>} Combined results.
  */
 export function getAll<T>(
-  baseUrl: string,
-  params?: string,
-  results?: T[],
+  url: string,
+  results: T[] = [],
 ): Promise<T[]> {
-  const url = params ? `${baseUrl}?${params}` : baseUrl;
-  const baseResults = results ? results : [];
+  const config: AxiosRequestConfig = {};
 
-  return get<APIList<T>>(url).then((response) => {
-    const combinedResults = baseResults.concat(response.results);
+  if (results.length) { config.baseURL = ''; }
+
+  return get<APIList<T>>(url, config).then((response) => {
+    const combinedResults = [...results, ...response.results];
 
     if (!response.next) { return Promise.resolve(combinedResults); }
 
-    const nextParams = response.next.substring(response.next.indexOf('?' + 1));
-
-    return getAll(baseUrl, nextParams, combinedResults);
+    return getAll(response.next, combinedResults);
   });
 }
 
@@ -123,9 +79,9 @@ export function getAll<T>(
 export function post<T>(
   url: string,
   data?: any,
-  config?: APIConfig,
+  config: AxiosRequestConfig = {},
 ): Promise<T> {
-  return request<T>(_.assign({}, config, { url, data, method: 'post' }));
+  return request<T>({ ...config, url, data, method: 'post' });
 }
 
 /**
@@ -138,9 +94,9 @@ export function post<T>(
 export function put<T>(
   url: string,
   data?: any,
-  config?: APIConfig,
+  config: AxiosRequestConfig = {},
 ): Promise<T> {
-  return request<T>(_.assign({}, config, { url, data, method: 'put' }));
+  return request<T>({ ...config, url, data, method: 'put' });
 }
 
 /**
@@ -153,7 +109,7 @@ export function put<T>(
 export function patch<T>(
   url: string,
   data?: any,
-  config?: APIConfig,
+  config: AxiosRequestConfig = {},
 ): Promise<T> {
-  return request<T>(_.assign({}, config, { url, data, method: 'patch' }));
+  return request<T>({ ...config, url, data, method: 'patch' });
 }
